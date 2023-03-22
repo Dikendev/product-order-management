da7a041513405e133294ef2ad54ed9db67e249c7
Dikendev <diken.dev@gmail.com>
2023-03-21 23:26:18 -0300

    Initial commit

---
+++ src/test/java/com/system/productorder/ProductOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.productorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
