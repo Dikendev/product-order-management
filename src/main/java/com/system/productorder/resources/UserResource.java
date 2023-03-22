e5a9d681667c05a1e5b41f85862fa9c9f3d98469
Dikendev <diken.dev@gmail.com>
2023-03-22 12:57:54 -0300

    Service layer, component registration

--- src/main/java/com/system/productorder/resources/UserResource.java
+++ src/main/java/com/system/productorder/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.system.productorder.resources;
 
 import com.system.productorder.entities.User;
+import com.system.productorder.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User a = new User(1L, "Diego", "diken.dev@gmail.com",
-                "47991717381", "999999");
-        return ResponseEntity.ok().body(a);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
