d5afff032382c9eb054fc47609fafdda5ef16c59
Dikendev <diken.dev@gmail.com>
2023-03-23 20:32:17 -0300

    implemented Product Set object values.

--- src/main/java/com/system/productorder/entities/Category.java
+++ src/main/java/com/system/productorder/entities/Category.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -18,17 +20,26 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @Transient
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {}
+
     public Category(Long id, String name) {
         super();
         this.id = id;
         this.name = name;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result * ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
 
