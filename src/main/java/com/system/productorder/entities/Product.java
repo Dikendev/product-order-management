252d42e4b15043247e562cf1205bd1126e4d95b4
Dikendev <diken.dev@gmail.com>
2023-03-24 23:56:00 -0300

    Product, OrderItem one-to-many association

--- src/main/java/com/system/productorder/entities/Product.java
+++ src/main/java/com/system/productorder/entities/Product.java
@@ -1,5 +1,6 @@
 package com.system.productorder.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -28,6 +29,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     private Product() {}
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -37,6 +41,15 @@ public class Product implements Serializable {
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
