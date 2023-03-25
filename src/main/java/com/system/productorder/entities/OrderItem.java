8f5d05516d7525c326cdda148ea4cee0cba6ac11
Dikendev <diken.dev@gmail.com>
2023-03-24 23:16:44 -0300

    added OrderItem entity

---
+++ src/main/java/com/system/productorder/entities/OrderItem.java
@@ -0,0 +1,59 @@
+package com.system.productorder.entities;
+
+import com.system.productorder.entities.pk.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPk id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){}
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderItem other = (OrderItem) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
